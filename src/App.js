7de932b30e000c12b7fbf15b298f72650f7cba90
DmitriIli <di.grebenev@outlook.com>
2025-05-27 14:40:38 +0300

    add button onclose

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useEffect } from 'react';
 import './App.css';
 import Calendar from './components/Calendar';
 
+tg = window.Telegram.webApp;
+
 function App() {
+
+  useEffect(() => {
+    tg.ready();
+   }, [])
+
+  const onClose = () => {
+    tg.close();
+  }
+
   return (
     <div className="App">
       <Calendar />
+      <button onClick={onClose}>Close</button>
     </div>
   );
 }
