cabd7ccfccbbd990ca105cee8ef1fb11aab9909f
DmitriIli <di.grebenev@outlook.com>
2025-05-28 10:38:53 +0300

    add  tg

--- src/App.js
+++ src/App.js
@@ -2,28 +2,30 @@ import { useEffect } from 'react';
 import './App.css';
 import Calendar from './components/Calendar';
 
-// tg = window.Telegram.WebApp;
 
-function App() {
 
-  // useEffect(() => {
-  //   tg.ready();
-  //  }, [])
+   tg = window.Telegram.WebApp;
 
-  const onClose = () => {
-    // tg.close();
+  function App() {
+
+    useEffect(() => {
+      tg.ready();
+     }, [])
+
+    const onClose = () => {
+      tg.close();
+    }
+
+    return (
+      <div className="App">
+        <Calendar />
+        <button onClick={onClose}>
+          <h1>
+            Close
+          </h1>
+        </button>
+      </div>
+    );
   }
 
-  return (
-    <div className="App">
-      <Calendar />
-      <button onClick={onClose}>
-        <h1>
-          Close
-        </h1>
-      </button>
-    </div>
-  );
-}
-
-export default App;
+  export default App;
